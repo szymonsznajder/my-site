8a3dd5b6792503828415e27bdf5d7786f8e359ec
Szymon Sznajder <sznajder.szymon@gmail.com>
2024-01-15 22:27:18 +0000

    check

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const quoteDiv = block.quoteSelector(':scope > div > div');
-  console.log(quoteDiv);
-  
+  console.log(quoteDiv, "dupa blada");
+
   const blockquote = document.createElement(tagName: 'blockquote');
 
 }
