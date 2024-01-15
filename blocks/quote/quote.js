b7b6ce30e8ddc190d825e68404856ec5085ce429
Szymon Sznajder <sznajder.szymon@gmail.com>
2024-01-15 22:52:43 +0000

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
   const quoteDiv = block.querySelector(':scope > div > div');
   console.log(quoteDiv);
 
-  const blockquote = document.createElement('blockquote');
+  const blockquote = document.createElement(tagName:'blockquote');
 
   blockquote.innerHTML = `${quoteDiv.innerHTML}`;
 
