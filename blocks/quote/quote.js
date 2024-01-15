bb49f0b0a1de01ec16fe6229e589870bcac71a2f
Szymon Sznajder <sznajder.szymon@gmail.com>
2024-01-15 22:57:10 +0000

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -7,4 +7,11 @@ export default function decorate(block) {
   blockquote.innerHTML = `${quoteDiv.innerHTML}`;
 
   quoteDiv.parentElement.replaceWith(blockquote);
+
+  const authorDiv = block.querySelector(':scope > div > div');
+  if(authorDiv){
+    const p = document.createElement(tagName: 'p');
+    p.innerHTML = `<em> - ${authorDiv.innerText}</em>`;
+    authorDiv.parentElement.replaceWith(p)
+  }
 }
