d775686945e5fef2a8c750d84e53c0324a6ddf73
Szymon Sznajder <sznajder.szymon@gmail.com>
2024-01-15 22:02:38 +0000

    add quote compoennt

---
+++ blocks/quote/quote.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+  const quoteDiv = block.quoteSelector(':scope > div > div');
+  console.log(quoteDiv);
+  
+  const blockquote = document.createElement(tagName: 'blockquote');
+
+}
