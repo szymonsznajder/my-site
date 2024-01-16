6dd97a2302e6bef2126b600ecadcd36246891af4
Szymon Sznajder <sznajder.szymon@gmail.com>
2024-01-16 22:15:20 +0000

    amend hero

---
+++ blocks/hero/hero.js
@@ -0,0 +1,4 @@
+export default function hero(block) {
+  const heroDiv = block.querySelector(':scope > h3');
+  console.log(heroDiv);
+}
--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
   const quoteDiv = block.querySelector(':scope > div > div');
-  console.log(quoteDiv);
 
   const blockquote = document.createElement('blockquote');
 
